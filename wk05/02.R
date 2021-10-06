b64aae739489af775e758f2a10de8ce8c7e985ea
Hedrav Bands <hr19ut@brocku.ca>
2021-10-06 10:07:26 -0400

    Add files via upload

--- wk05/02.R
+++ wk05/02.R
@@ -6,7 +6,7 @@ choose(n,k)*p^k*(1-p)^(n-k)
 # 0.0778
 
 #b X >= 4
-seq <-c(4,5)
+seq <- c(4,5)
 sum <- 0
 for (k in seq){
   sum <- sum + choose(n,k)*p^k*(1-p)^(n-k)
--- wk05/03.R
+++ wk05/03.R
@@ -6,9 +6,9 @@ p <- 0.99
 1 - choose(n,k)*p^k*(1-p)^(n-k)
 # 0.0956
 
-#b 
-seq <- c(0,1,2,3,4)
-p <- (0.10 + 0.05 + 0.01) #fail
+#b X < 5 Fail  or X >= 5 success
+seq <- c(5,6,7,8,9,10)
+p <- 1 - (0.10 + 0.05 + 0.01) #success
 sum <- 0
 for (k in seq){
   sum <- sum + choose(n,k)*p^k*(1-p)^(n-k)
@@ -17,6 +17,6 @@ sum
 # 0.9870
 
 #c X = 1
-
-print(expectedValue <- n*0.1)
+p1<- 0.1
+print(expectedValue <- n*p1)
 # 1
